2a7cf8cabb1021b4ad4bdc59f796822187245d25
Mahmudul Islam Prakash <prakash@seazoobd.com>
2020-12-04 23:01:35 +0600

    2nd commit

--- index.php
+++ index.php
@@ -6,61 +6,50 @@
 	<title>Task 1</title>
 </head>
 <body>
-	<table border="1" cellspacing="0">
-		<thead>
-			<tr>
-				<th>Category Name</th>
-				<th>Total Items</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-				include "Class\Task1.php";
+	<?php
+		include "Class\Task1.php";
 
-				function createChild($categories, $parentId = 0, $lavel = 0)
-				{
-				    $results = [];
-				    foreach($categories as $category) {
-				        if ($parentId == $category['ParentId']) {
-				            $nextParentId = $category['Id'];
-				            
-				            for ($i=0; $i < $lavel; $i++) { 
-				            	echo "&nbsp&nbsp&nbsp&nbsp";
-				            }
-				            echo $category['Name']."<br>";
-				            $lavel++;
-				            $category['Child'] = createChild($categories, $nextParentId, $lavel);
-				            $results[] = $category;
-				        }
-				    }
-				    return $results;
-				}
+		function createChild($categories, $parentId = 0, $lavel = 0)
+		{
+		    $results = [];
+		    foreach($categories as $category) {
+		        if ($parentId == $category['ParentId']) {
+		            $nextParentId = $category['Id'];
+		            
+		            for ($i=0; $i < $lavel; $i++) { 
+		            	echo "&nbsp&nbsp&nbsp&nbsp";
+		            }
+		            echo $category['Name']."<br>";
+		            $lavel++;
+		            $category['Child'] = createChild($categories, $nextParentId, $lavel);
+		            $results[] = $category;
+		        }
+		    }
+		    return $results;
+		}
 
-				$task1 = new Task1();
-				$result = $task1->get_parent_category();
-				$array_data = array();
-				if ($result->num_rows > 0) {
-					while($row = $result->fetch_assoc()) {
-						$array_data[] = $row;
-					}
-					echo "<pre>";
+		$task1 = new Task1();
+		$result = $task1->get_parent_category();
+		$array_data = array();
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				$array_data[] = $row;
+			}
+			echo "<pre>";
 
-					$result = createChild($array_data);
-					print_r($result);
+			$result = createChild($array_data);
+			// print_r($result);
 
 
 
-				} else {
-					?>
-					<tr>
-						<td colspan="2">Not found!</td>
-					</tr>
-					<?php
-				}
+		} else {
 			?>
-		</tbody>
-	</table>
-
+			<tr>
+				<td colspan="2">Not found!</td>
+			</tr>
+			<?php
+		}
+	?>
 </body>
 </html>
 
